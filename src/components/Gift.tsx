01e3c7eb7dcb264a4a82f58324a4acf52f4a59e5
eeldinoyooh <dinobkt123@gmail.com>
2025-12-06 04:44:52 +0700

    Revert background to dark and fix subpixel gap with wave restoration

--- src/components/Gift.tsx
+++ src/components/Gift.tsx
@@ -77,8 +77,13 @@ export const Gift: React.FC = () => {
           ))}
         </div>
 
-        {/* Kirim Kado Fisik (Optional) */}
-        {/* <div className="mt-12 bg-[#1a202c] p-10 rounded-[3rem] border-4 border-white ring-1 ring-gray-200 shadow-2xl" data-aos="fade-up" data-aos-delay="200">
+        <div className="absolute bottom-[-1px] left-0 w-full leading-none z-20 pointer-events-none">
+          <svg className="relative block w-full h-[60px] md:h-[100px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
+            <path fill="#1a202c" fillOpacity="0.3" d="M0,160L48,176C96,192,192,224,288,224C384,224,480,192,576,165.3C672,139,768,117,864,128C960,139,1056,181,1152,197.3C1248,213,1344,203,1392,197.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+            <path fill="#1a202c" fillOpacity="0.7" d="M0,224L48,213.3C96,203,192,181,288,181.3C384,181,480,203,576,224C672,245,768,267,864,250.7C960,235,1056,181,1152,165.3C1248,149,1344,171,1392,181.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+            <path fill="#1a202c" fillOpacity="1" d="M0,256L48,261.3C96,267,192,277,288,266.7C384,256,480,224,576,213.3C672,203,768,213,864,229.3C960,245,1056,267,1152,266.7C1248,267,1344,245,1392,234.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+          </svg>
+        </div>        {/* <div className="mt-12 bg-[#1a202c] p-10 rounded-[3rem] border-4 border-white ring-1 ring-gray-200 shadow-2xl" data-aos="fade-up" data-aos-delay="200">
           <h3 className="font-display italic text-2xl text-[#D4C3A3] mb-4">Alamat Pengiriman Kado</h3>
           <p className="text-gray-300 text-sm leading-relaxed">
             {graduationConfig.giftAddress}
@@ -86,14 +91,7 @@ export const Gift: React.FC = () => {
         </div> */}
       </div>
 
-      {/* Wave Transition to RSVP (Dark Navy) */}
-      <div className="absolute bottom-[-1px] left-0 w-full leading-none z-20 pointer-events-none">
-        <svg className="relative block w-full h-[60px] md:h-[100px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
-          <path fill="#1a202c" fillOpacity="0.3" d="M0,160L48,176C96,192,192,224,288,224C384,224,480,192,576,165.3C672,139,768,117,864,128C960,139,1056,181,1152,197.3C1248,213,1344,203,1392,197.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-          <path fill="#1a202c" fillOpacity="0.7" d="M0,224L48,213.3C96,203,192,181,288,181.3C384,181,480,203,576,224C672,245,768,267,864,250.7C960,235,1056,181,1152,165.3C1248,149,1344,171,1392,181.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-          <path fill="#1a202c" fillOpacity="1" d="M0,256L48,261.3C96,267,192,277,288,266.7C384,256,480,224,576,213.3C672,203,768,213,864,229.3C960,245,1056,267,1152,266.7C1248,267,1344,245,1392,234.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-        </svg>
-      </div>
+
     </section>
   );
 };
