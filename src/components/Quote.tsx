0b6c888413680c1a71869456979b468e2d76ade2
eeldinoyooh <dinobkt123@gmail.com>
2025-12-06 04:30:53 +0700

    Fix subpixel gap in Quote component

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -23,7 +23,7 @@ export const Quote: React.FC = () => {
       </div>
 
       {/* Multi-layer Wave Transition to BrideGroom (White) */}
-      <div className="absolute bottom-0 left-0 w-full leading-none z-20">
+      <div className="absolute bottom-[-1px] left-0 w-full leading-none z-20">
         <svg className="relative block w-full h-[100px] md:h-[150px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
           <path fill="#ffffff" fillOpacity="0.3" d="M0,160L48,176C96,192,192,224,288,224C384,224,480,192,576,165.3C672,139,768,117,864,128C960,139,1056,181,1152,197.3C1248,213,1344,203,1392,197.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
           <path fill="#ffffff" fillOpacity="0.7" d="M0,224L48,213.3C96,203,192,181,288,181.3C384,181,480,203,576,224C672,245,768,267,864,250.7C960,235,1056,181,1152,165.3C1248,149,1344,171,1392,181.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
